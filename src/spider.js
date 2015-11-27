c621c588f9055dce8545fb75fe7ab6aece00d6dd
zhuyingda <383329144@qq.com>
2015-11-27 21:52:50 +0800

    初步抓取xhr接口

--- src/spider.js
+++ src/spider.js
@@ -2,45 +2,46 @@
 var fs = require('fs');
 var through = require('through2');
 var request = require('request');
+
 var hostPrefix = '';
 var urlStack = [];
-var jsCount=0;
+var jsCount = 0;
 var jsStack = [];
+var xhrCount = 0;
 
-function httpGet(url, ii) {
+function httpGet(url, errStr) {
     //console.log('发起请求:', url);
     request
         .get(url)
-        .on('response', function (response) {
-            //console.log(response.statusCode);
-            //console.log(response.headers['content-type']);
-        })
         .on('error', function (err) {
-            console.log('出错url:', url);
-            console.log(err);
-            console.log('出错时扫到的:', ii);
+            //console.log('出错url:', url);
+            //console.log(err);
+            //console.log('出错时扫到的:', errStr);
         })
         .pipe(filter())
 }
 
+function httpGetJs(src){
+    request.get(src).pipe(detectXhr());
+}
+
 function filter() {
-    return through.obj(function (file, enc, cb) {
-        //console.log('result:', file.toString());
+    return through.obj({ objectMode: true, allowHalfOpen: false },function (file, enc, cb) {
         let htmlContent = file.toString();
         detectPath(htmlContent);
         detectJs(htmlContent);
+        cb();
     })
 }
 
 function detectPath(str) {
     let reg = /<a\b("[^"]*"|'[^']*'|[^'">])*>/g;
     str.replace(reg, function (i) {
-
         let reg2 = /href=["'].*?["']/g;
         i.replace(reg2, function (ii) {
             let url = ii.slice(6, ii.length - 1);
             //console.log('发现路径:', url);
-            if (/^http/.test(url)|| /^git/.test(url)) {
+            if (/^http/.test(url) || /^git/.test(url) || /^javascript/.test(url)) {
                 //console.log('忽略外站url:', url);
                 return;
             }
@@ -53,7 +54,6 @@ function detectPath(str) {
             urlStack.push(url);
             httpGet(hostPrefix + url, ii);
         })
-
     });
 }
 
@@ -62,8 +62,13 @@ function detectJs(str) {
     str.replace(reg, function (i) {
         let reg2 = /src=["'].*?["']/g;
         i.replace(reg2, function (ii) {
-            var src = ii.slice(5, ii.length-1);
+            var src = ii.slice(5, ii.length - 1);
             for (let j = 0; j < jsStack.length; j++) {
+                if (/^http/.test(src)) {
+                    //console.log('发现外站js:', url);
+                    httpGetJs(src);
+                    return;
+                }
                 if (jsStack[j] == src) {
                     //console.log('忽略已扫src:', url);
                     return;
@@ -72,22 +77,31 @@ function detectJs(str) {
             jsStack.push(src);
             jsCount++;
             console.log('发现js:', src);
+            httpGetJs(hostPrefix + src);
         })
     });
 }
 
-function detectAjax(str) {
-
+function detectXhr(){
+    return through.obj({ objectMode: true, allowHalfOpen: false },function (file, enc, cb) {
+        let jsContent = file.toString();
+        let reg = /["']\/\w*['"]/g;
+        jsContent.replace(reg, function (token) {
+            console.log('发现疑似ajax接口:', token)
+            xhrCount++;
+        });
+        cb();
+    })
 }
 
 process.on('exit', function (code) {
-    console.timeEnd('spider process');
-    console.log('About to exit with code:', code);
-    console.log('共发现js地址:'+ jsCount +'个');
+    console.timeEnd('共消耗时间');
+    //console.log('About to exit with code:', code);
+    console.log('共发现xhr地址:' + xhrCount + '个');
 });
 
 function main(host) {
-    console.time('spider process');
+    console.time('共消耗时间');
     let url = 'http://' + host;
     hostPrefix = url;
     httpGet(url);
--- test.js
+++ test.js
@@ -1,21 +1,4 @@
 "use strict";
-//var spawn = require('child_process').spawn,
-//    spider = spawn('node',['src/spider.js', '--host', 'www.zhuyingda.com']);
-//
-//console.time('spider time:');
-//
-//spider.stdout.on('data', function (data) {
-//    console.log('stdout: ' + data);
-//});
-//
-//spider.stderr.on('data', function (data) {
-//    console.log('stderr: ' + data);
-//});
-//
-//spider.on('close', function (code) {
-//    console.log('child process exited with code ' + code);
-//    console.timeEnd('spider time:');
-//});
 var spider = require('./src/spider');
 
-spider('www.imququ.com');
+spider('zhuyingda.xiao.youxi.com');
