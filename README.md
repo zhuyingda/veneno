f3b51bd75c75e37125629c0bfdfb26c7ed9cb7fe
zhuyingda <383329144@qq.com>
2015-12-09 01:17:27 +0800

    根据watchlist和apilist进行xss可疑扫描

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # veneno
-a web security tool written by NodeJs.
+- a web security tool written by NodeJs.
+- 基于NodeJs编写的web渗透工具
+- 目前进度：XSS自动化流程构建中...
--- src/xss.js
+++ src/xss.js
@@ -4,40 +4,101 @@ var through = require('through2');
 var request = require('request');
 var fs = require('fs');
 
-var data;
+var leaks = [];
+var watchList = [
+    {
+        url: 'http://127.0.0.1:3000/get_comment'
+    },
+    {
+        url: 'http://127.0.0.1:3000'
+    }
+];
+var apiList = [
+    {
+        url: 'http://127.0.0.1:3000/add_comment',
+        params: [
+            'content'
+        ]
+    },
+    {
+        url: 'http://127.0.0.1:3000/get_comment',
+        params: [
+
+        ]
+    }
+];
+var testTokenReg = /tokenMadeByVenenoWebPenetration/;
+var testTokenStr = 'tokenMadeByVenenoWebPenetration';
 
 function loadDict() {
+    console.log('读取字典...');
     return new Promise((resolve, reject) => {
         fs.createReadStream(__dirname + '/dict.json')
             .pipe(through(function (chunk, enc, callback) {
                 let reg1 = /\\"/g;
                 let reg2 = /transform\\/g;
-                data = chunk.toString();
-                //data = data.replace(/\\"/g,'\"');
-                //console.log(data);
+                let data = chunk.toString();
                 data = JSON.parse(data);
-                //console.log(data);
                 let temp = [];
                 data.dict.forEach(function (i) {
                     i = i.replace(reg1, '"');
                     i = i.replace(reg2, '\\');
                     temp.push(i);
                 });
-                data = temp;
-                resolve();
+                console.log('字典读取完成');
+                resolve(temp);
                 callback()
             }));
     })
 }
 
-function inject() {
-    console.log(data);
+function httpGet(url, params) {
+    return new Promise((resolve, reject)=> {
+        //console.log(url);
+        request
+            .get(url, {qs: params})
+            .on('error', function (err) {
+                console.log(err);
+                reject(err)
+            })
+            .pipe(through.obj({objectMode: true, allowHalfOpen: false}, (file, enc, cb)=> {
+                let response = file.toString();
+                //console.log(response);
+                resolve(response);
+                cb();
+            }))
+    })
 }
 
-function main() {
+function watch(originUrl, para) {
+    for (let i of watchList) {
+        request(i.url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                if (testTokenReg.test(body)) {
+                    console.log('发现疑似xss点:', originUrl, '参数:', para, '<=>', i.url);
+                }
+            }
+        })
+    }
+}
+
+function testApi() {
+    for (let i of apiList) {
+        for (let j of i.params) {
+            let o = {};
+            o[j] = testTokenStr;
+            httpGet(i.url, o).then((res)=> {
+                //console.log('向接口', i.url, '发起http请求成功');
+                watch(i.url, j);
+            })
+        }
+    }
+}
+
+function main(opt) {
     loadDict()
-        .then(()=> {
-            console.log(data);
+        .then((dict)=> {
+            testApi();
         })
 }
 
