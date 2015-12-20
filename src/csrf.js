08238aa79cf1f41dc598afe022a68c09e4860fc2
zhuyingda <383329144@qq.com>
2015-12-20 14:45:30 +0800

    抓取npm未被占用的包名

---
+++ src/csrf.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const request = require('request');
+const output = require('../lib/out');
+
+function httpGet(url) {
+    return new Promise((resolve, reject) => {
+        request({url: url}, (error, response, body) => {
+            if (error == null) {
+                resolve(body);
+            } else {
+                output.err(error);
+                reject(error);
+            }
+        })
+    })
+}
+
+module.exports = {
+    httpGet: httpGet
+}
