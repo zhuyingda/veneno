ad05940c7192ea97cdf1d6a9513b24aae154a486
Sugar <yingdazhu@icloud.com>
2015-12-21 16:02:59 +0800

    Update csrf.js

--- test/csrf.js
+++ test/csrf.js
@@ -2,6 +2,7 @@
 const csrf = require('../src/csrf');
 const word = require('../lib/word_corpus');
 const loki = require('lokijs');
+const https = require('https');
 
 let name = 'a';
 let db = new loki('names.json');
@@ -13,12 +14,35 @@ function save(val) {
 }
 
 function testName(name) {
-    console.log('发起请求：',name);
-    csrf.httpGet('http://www.npmjs.com/search?q=' + name).then(function (data) {
-        if (/Sorry,\s*no\s*results\s*for/.test(data)) {
-            console.log(name, '可用');
-            save(name);
-        }
+    //console.log('发起请求：',name);
+    //csrf.httpGet('http://www.npmjs.com/search?q=' + name).then(function (data) {
+    //    if (/Sorry,\s*no\s*results\s*for/.test(data)) {
+    //        console.log(name, '可用');
+    //        save(name);
+    //    }
+    //});
+    let options = {
+        hostname: 'www.npmjs.com',
+        port: 443,
+        path: '/search?q=jalor',
+        method: 'GET'
+    };
+
+    let req = https.request(options, function(res) {
+        console.log("statusCode: ", res.statusCode);
+        console.log("headers: ", res.headers);
+
+        res.on('data', function(d) {
+            if (/Sorry,\s*no\s*results\s*for/.test(d)) {
+                console.log(name, '可用');
+                save(name);
+            }
+        });
+    });
+    req.end();
+
+    req.on('error', function(e) {
+        console.error(e);
     });
 }
 
