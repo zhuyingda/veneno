cf62c9493e8462bf2787736edcab3f11a4d4cab8
zhuyingda <383329144@qq.com>
2015-12-21 14:56:59 +0800

    完成csrf对npm包名的扫描

--- test/csrf.js
+++ test/csrf.js
@@ -1,73 +1,34 @@
 "use strict";
-var csrf = require('../src/csrf');
+const csrf = require('../src/csrf');
+const word = require('../lib/word_corpus');
+const loki = require('lokijs');
+
+let name = 'a';
+let db = new loki('names.json');
+let names = db.addCollection('untapped_names');
+
+function save(val) {
+    names.insert( { name : val} );
+    db.saveDatabase();
+}
 
 function testName(name) {
+    console.log('发起请求：',name);
     csrf.httpGet('http://www.npmjs.com/search?q=' + name).then(function (data) {
         if (/Sorry,\s*no\s*results\s*for/.test(data)) {
             console.log(name, '可用');
+            save(name);
         }
     });
 }
 
-function carry(l) {
-    let asc = l.charCodeAt(0);
-    if (asc === 122) {
-        return false;
-    }
-    return String.fromCharCode(asc + 1);
-}
-
-function setStringAt(str, i, l) {
-    if (i === 0) {
-        return l + str.slice(1, str.length)
-    } else if (i === str.length - 1) {
-        return str.slice(0, str.length - 1) + l;
-    } else {
-        return str.slice(0, i) + l + str.slice(i + 1, str.length);
+function run() {
+    if(name != 'z'){
+        console.log(name);
+        testName(name);
+        name = word.nextWord(name);
+        process.nextTick(run);
     }
 }
 
-function isZZZ(word) {
-    for (let i of word) {
-        if (i != 'z') {
-            return false;
-        }
-    }
-    return true;
-}
-
-function nextWord(word) {
-    let tempStr = word;
-    if (isZZZ(tempStr)) {
-        let newWord = '';
-        for (let i of word) {
-            newWord += 'a';
-        }
-        newWord += 'a';
-        return newWord;
-    }
-    if (word[word.length - 1] != 'z') {
-        return setStringAt(word, word.length - 1, carry(tempStr[word.length - 1]));
-    }
-    let index = tempStr.length - 1;
-    while (index >= 0) {
-        if (tempStr.charCodeAt(index) < 122) {
-            tempStr = setStringAt(tempStr, index, carry(tempStr[index]));
-            let i = 1;
-            while (index + i < word.length) {
-                tempStr = setStringAt(tempStr, index + i, 'a');
-                i++;
-            }
-            break;
-        } else {
-            index--;
-        }
-    }
-    return tempStr;
-}
-
-let name = 'a';
-while(name != 'zzzzz'){
-    console.log(name);
-    name = nextWord(name);
-}
+run();
