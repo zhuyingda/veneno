3eb14b698edd2bce798cd4a2f688518f8a5d7df7
zhuyingda <383329144@qq.com>
2015-12-22 16:03:57 +0800

    加入一个xss沙箱，可以在本地3000端口测试自己的xss

---
+++ sandbox/routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+var store = {
+    comments: [
+        {
+            comment: '第一条评论要先写进去',
+            time: 'Tue Dec 08 2015'
+        }
+    ]
+};
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {comments: store.comments});
+}).get('/add_comment', function (req, res, next) {
+    var d = new Date();
+    store.comments.push({
+        comment: req.query.content,
+        time: d.toDateString() + ':' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds()
+    });
+    res.end(JSON.stringify({errno: 0, errmsg: 'ok', comments: store.comments}));
+}).get('/get_comment', function (req, res, next) {
+    res.end(JSON.stringify({errno: 0, errmsg: 'ok', comments: store.comments}));
+})
+
+module.exports = router;
